11618d055f21ecbf88bd953f134e08304f074d7a
SAKKalinduS <sankalpa5285@gmail.com>
2025-01-18 12:01:00 +0530

    listing backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import CreateListingPage from './pages/CreateListing';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/login" element={<LoginPage/>} />
       <Route path="/home" element={<HomePage/>} />
       <Route path="/register" element={<RegisterPage/>} />
+      <Route path="/create-listing" element={<CreateListingPage/>} />
     </Routes>
     </BrowserRouter>
     </div>
